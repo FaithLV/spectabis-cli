873406eec6bb93c16052a9c08132e701429a4c97
Valters <infinity1188@yahoo.com>
2018-08-19 23:00:33 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spectabis_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
