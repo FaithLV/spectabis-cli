fdaaa7ccbd794ca4075e99448e67a34e70ef0ed7
Valters <infinity1188@yahoo.com>
2018-08-20 19:49:51 +0300

    Implement GameProfile listing

--- Models/HelpPrettyPrints.cs
+++ Models/HelpPrettyPrints.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using ConsoleTables;
@@ -28,6 +29,27 @@ namespace spectabis_cmd.Models
             PropertyInfo[] _props = typeof(GameProfile).GetProperties();
             string[] properties = _props.Select( x => x.Name.ToString()).ToArray(); //properties as strings
             ConsoleTable table = new ConsoleTable(properties);
+
+            //Create arrays for Console Table and populate it
+            for(int i = 0; i < profiles.Count; i++)
+            {
+                //object[] tableBuffer = new object[properties.Length];
+                List<object> tableBuffer = new List<object>();
+
+                foreach(PropertyDescriptor prop in TypeDescriptor.GetProperties(profiles[i]))
+                {
+                    Type t = profiles[0].GetType();
+                    PropertyInfo p = t.GetProperty(prop.Name);
+                    string value = (string)p.GetValue(profiles[i], null);
+
+                    /* System.Console.WriteLine($"{prop.Name} | {profiles[0].ProfileName} {profiles[0].GamePath} {profiles[0].GameSerial}");
+                    object obj = prop.GetType().GetProperty(prop.Name).GetValue(profiles[0].GetType(), null); */
+
+                    tableBuffer.Add(value);
+                }
+
+                table.AddRow(tableBuffer.ToArray());
+            }
 
             table.Write();
         }
