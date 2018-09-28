6e8e16f6689737432d251be978259bb1c48c11a0
Valters <infinity1188@yahoo.com>
2018-09-28 17:51:49 +0300

    Add profile info auto complete

--- AutoCompleteHandler.cs
+++ AutoCompleteHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using spectabis_cli.Domain;
 
 namespace spectabis_cli
 {
@@ -8,10 +9,20 @@ namespace spectabis_cli
         public char[] Separators { get; set; } = new char[] { ' ', '.', '/', '\\', ':' };
         public string[] GetSuggestions (string text, int index)
         {
+            //Base command hints
             if (string.IsNullOrWhiteSpace(text))
             {
                 return Program.CommandTable.Keys.ToArray();
             }
+
+            //Profile hints
+            if(text.StartsWith("profiles "))
+            {
+                string query = text.Replace("profiles ", String.Empty).ToLower();
+                var profiles = ProfileManager.GetAllProfiles().Where(x => x.ProfileID.ToLower().StartsWith(query));
+                return profiles.Select(x => x.ProfileID).ToArray();
+            }
+
             return null;
         }
 
