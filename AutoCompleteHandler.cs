69a9d543b0a0513b33629fa97f392560a4eb4ae5
Valters <infinity1188@yahoo.com>
2018-09-28 19:30:36 +0300

    Add proper auto completion for base commands

--- AutoCompleteHandler.cs
+++ AutoCompleteHandler.cs
@@ -6,6 +6,7 @@ namespace spectabis_cli
 {
     class AutoCompletionHandler : IAutoCompleteHandler
     {
+        private string[] CommandKeys = Program.CommandTable.Keys.ToArray();
         public char[] Separators { get; set; } = new char[] { ' ', '.', '/', '\\', ':' };
         public string[] GetSuggestions (string text, int index)
         {
@@ -15,15 +16,36 @@ namespace spectabis_cli
                 return Program.CommandTable.Keys.ToArray();
             }
 
+            string[] arguments = SmartParser.ParseAsArguments(text).ToArray();
+            string lastArg = arguments.Last();
+            bool newArgument = text.Last() == ' ';
+
+            if(arguments.Count() == 1 && !newArgument && CommandKeys.Any(x => x.StartsWith(lastArg)))
+            {
+                return QueryCommands(lastArg);
+            }
+
             //Profile hints
             if(text.StartsWith("profiles "))
             {
                 string query = text.Replace("profiles ", String.Empty).ToLower();
-                var profiles = ProfileManager.GetAllProfiles().Where(x => x.ProfileID.ToLower().StartsWith(query));
-                return profiles.Select(x => x.ProfileID).ToArray();
+                return QueryProfiles(query);
             }
 
+
+
             return null;
+        }
+        private string[] QueryProfiles(string query)
+        {
+            var profiles = ProfileManager.GetAllProfiles().Where(x => x.ProfileID.ToLower().StartsWith(query));
+            return profiles.Select(x => x.ProfileID).ToArray();
+        }
+
+        private string[] QueryCommands(string query)
+        {
+            var commands = CommandKeys.Where(x => x.StartsWith(query)).ToArray();
+            return commands;
         }
 
     }
