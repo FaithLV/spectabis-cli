5cb0bf10acc1978f79f51c55ebf85721ce593dc5
Valters <infinity1188@yahoo.com>
2018-10-05 00:36:24 +0300

    Add custom plugin path fields to GameProfile model

--- Model/GameProfile.cs
+++ Model/GameProfile.cs
@@ -12,6 +12,13 @@ namespace spectabis_cli.Model
         public string GameSerial {get; set;}
         public string ImagePath{get; set;}
         public string CustomExecutablePath {get; set;}
+        public string GS_Plugin {get; set;}
+        public string PAD_Plugin {get; set;}
+        public string SPU2_Plugin {get; set;}
+        public string CDVD_Plugin {get; set;}
+        public string USB_Plugin {get; set;}
+        public string FW_Plugin {get; set;}
+        public string DEV9_Plugin {get; set;}
         public GameProfile() { }
 
         public string GenerateHash()
